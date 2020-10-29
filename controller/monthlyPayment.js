628a2e4371ccb9281bedfc58378ca35c11250292
Induwara-Nagodavithana <induwara.nagodavithana@gmail.com>
2020-10-29 14:49:27 +0530

    mo11

--- controller/monthlyPayment.js
+++ controller/monthlyPayment.js
@@ -5,9 +5,6 @@ var PriceDetail = require("../controller/priceDetail");
 
 
 
-
-
-
 module.exports.createMonthlyPayment = function (date, callback) {
     console.log("Create Payment");
 
@@ -18,7 +15,7 @@ module.exports.createMonthlyPayment = function (date, callback) {
 
     var id = 1;
     var price;
-   PriceDetail.getPriceDetailById(id, function(err, priceDetail){
+   await PriceDetail.getPriceDetailById(id, function(err, priceDetail){
     if (err) {
       console.log("errors" + err.message);
       callback(err);
@@ -89,3 +86,86 @@ module.exports.createMonthlyPayment = function (date, callback) {
   
     
   };
+
+
+// module.exports.createMonthlyPayment = function (date, callback) {
+//     console.log("Create Payment");
+
+//     var accId=1;
+//     var flag=0;
+//     // var datetime = new Date();
+//     //     console.log(datetime.toISOString().slice(0,7));
+
+//     var id = 1;
+//     var price;
+//    PriceDetail.getPriceDetailById(id, function(err, priceDetail){
+//     if (err) {
+//       console.log("errors" + err.message);
+//       callback(err);
+//     //   res.sendStatus(400);
+//       return;
+//     } else {
+//         console.log("Start");
+//       console.log(priceDetail);
+//       console.log("12312312dgd");
+//       console.log(priceDetail.dataValues);
+//       console.log("12312312dgd");
+//       console.log(priceDetail.dataValues.price);
+//       console.log("Finish");
+//       price=priceDetail.dataValues.price;
+//     //   res.setHeader("Content-Type", "application/json");
+//     //  // res.body(employee);
+//     //   res.end(JSON.stringify({ priceDetail: priceDetail}));
+//     }
+//   }).then((price) => {
+//        // for ( flag = 0; flag < 5; accId++,flag++) {
+//         var monthlyPay=0;
+//         Usage.getSumUsageByDate(accId,date, function(err, usage){
+//             if (err) {
+//               console.log("errors" + err.message);
+//               callback(err);
+//             //   res.sendStatus(400);
+//               return;
+//             } else {
+//                 console.log("Payment Start");
+//                 console.log(price);
+//               console.log(usage);
+//               monthlyPay = usage*price;
+//               console.log(monthlyPay);
+//               console.log("Payment xvcv");
+
+//               var newPayment = {
+//                 paymentData: monthlyPay,
+//                 accountId: accId
+//               };
+            
+//               Payment.createPayment(newPayment, function (err, payment) {
+//                 if (err) {
+//                   console.log("errors " + err.message);
+//                   callback(err);
+//                 //   res.sendStatus(400);
+//                   return;
+//                 } else {
+//                   console.log(payment);
+//                   callback(null, payment);
+//                 //   res.setHeader("Content-Type", "application/json");
+//                 //   res.end(JSON.stringify({ payment: payment}));
+//                 }
+//               });
+
+
+//             //   res.setHeader("Content-Type", "application/json");
+//             //  // res.body(employee);
+//             //   res.end(JSON.stringify({ usage: usage}));
+//             }
+//           });
+        
+//     // }
+//         }).catch((err) => {
+//             callback(err);
+//           });
+  
+   
+  
+    
+//   };
