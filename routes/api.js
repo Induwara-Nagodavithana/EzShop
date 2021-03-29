47aa8de120c7495c2d1f3196b2b417f3dee7bd2b
Induwara-Nagodavithana <induwara.nagodavithana@gmail.com>
2021-03-29 20:07:42 +0530

    SellerOrder Added

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,7 @@ var User = require("../controller/user");
 var Item = require("../controller/item");
 var Order = require("../controller/order");
 var Cart = require("../controller/cart");
+var SellerOrder = require("../controller/seller_order");
 var connectDB = require("../config/database");
 
 //var passport = require("passport");
@@ -28,12 +29,12 @@ const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
     cb(null, true);
   } else {
-    cb(new Error ('Image unacceptable!'), false);
+    cb(new Error('Image unacceptable!'), false);
   }
 }
 
 const upload = multer({
-  storage: storage, 
+  storage: storage,
   limits: {
     fileSize: 1024 * 1024 * 5
   },
@@ -119,6 +120,25 @@ router.post("/registerUser", urlencodedParser, function (req, res) {
       return;
     } else {
       console.log(user);
+      var CID = user._id;
+      var items = [];
+
+      var newCart = {
+        customer_id: CID,
+        items: items,
+      };
+
+      Cart.createCart(newCart, function (err, cart) {
+        if (err) {
+          console.log("errors" + err);
+          res.sendStatus(400);
+          return;
+        } else {
+          console.log(cart);
+          res.setHeader("Content-Type", "application/json");
+          res.end(JSON.stringify({ cart: cart }));
+        }
+      });
       res.setHeader("Content-Type", "application/json");
       res.end(JSON.stringify({ user: user }));
     }
@@ -293,12 +313,12 @@ router.post("/registerItem", urlencodedParser, upload.single('image'), function 
 });
 
 
-router.post("/updateItem", urlencodedParser,upload.single('image'), function (req, res) {
+router.post("/updateItem", urlencodedParser, upload.single('image'), function (req, res) {
 
   var imageName2 = null;
-  if (req.file!=null) {
+  if (req.file != null) {
     // upload.single('image');
-    imageName2=req.file.path;
+    imageName2 = req.file.path;
   }
 
   var name = req.body.name;
@@ -439,18 +459,61 @@ router.post("/registerOrder", function (req, res) {
     order_date: date,
   };
 
-  Order.createOrder(newOrder, function (err, order) {
+  Order.createOrder(newOrder, function (err, order1) {
     if (err) {
       console.log("errors" + err);
       res.sendStatus(400);
       return;
     } else {
-      console.log(order);
+      console.log(order1);
+      order1.order.forEach(addSellerOrder);
+      console.log("order1.order " + order1.order);
+      function addSellerOrder(item3, index, arr) {
+        console.log("item " + item3);
+        console.log("item.item_id " + item3.item_id);
+        Item.findOneItem(item3.item_id, function (err, item2) {
+          if (err) {
+            console.log("errors" + err);
+            // res.sendStatus(400);
+            return;
+          } else {
+            console.log(item2);
+            var SID = item2.seller_id;
+            var CID = order1.customer_id;
+            var item = item2._id;
+            var qty = item3.qty;
+            var isShipped = false;
+            var date = order1.order_date;
+
+            var newOrder = {
+              seller_id: SID,
+              customer_id: CID,
+              item: item,
+              qty: qty,
+              isShipped: isShipped,
+              order_date: date,
+            };
+
+            SellerOrder.createSellerOrder(newOrder, function (err, order) {
+              if (err) {
+                
+                // res.setHeader("Content-Type", "application/json");
+                // res.end("Error Creating Order");
+                return;
+              } else {
+                console.log(order);
+                
+              }
+            });
+            // res.setHeader("Content-Type", "application/json");
+            // res.end(JSON.stringify({ item: item }));
+          }
+        });
+      }
       res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify({ order: order }));
+      res.end(JSON.stringify({ order: order1 }));
     }
   });
-
 });
 
 
@@ -640,5 +703,165 @@ router.post("/deleteCart", urlencodedParser, function (req, res) {
     }
   });
 });
+
+///////////////// Seller Order Api /////////////////////////////
+
+router.post("/registerSellerOrder", urlencodedParser, function (req, res) {
+
+  var SID = req.body.seller_id;
+  var CID = req.body.customer_id;
+  var item = req.body.item;
+  var qty = req.body.qty;
+  var isShipped = req.body.isShipped;
+  var date = req.body.order_date;
+
+  var newOrder = {
+    seller_id: SID,
+    customer_id: CID,
+    item: item,
+    qty: qty,
+    isShipped: isShipped,
+    order_date: date,
+  };
+
+  SellerOrder.createSellerOrder(newOrder, function (err, order) {
+    if (err) {
+      console.log("errors" + err);
+      res.sendStatus(400);
+      return;
+    } else {
+      console.log(order);
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ order: order }));
+    }
+  });
+
+});
+
+
+router.get('/getAllSellerOrder', async (req, res) => {
+  SellerOrder.findAllSellerOrder(function (err, order) {
+    if (err) {
+      console.log("errors" + err);
+      res.sendStatus(400);
+      return;
+    } else {
+      console.log(order);
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ order: order }));
+    }
+  });
+});
+
+router.post('/getOneSellerOrderById', urlencodedParser, async (req, res) => {
+  SellerOrder.findOneSellerOrder(req.body.id, function (err, order) {
+    if (err) {
+      console.log("errors" + err);
+      res.sendStatus(400);
+      return;
+    } else {
+      console.log(order);
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ order: order }));
+    }
+  });
+});
+
+
+router.post('/getAllSellerOrderBySeller', urlencodedParser, async (req, res) => {
+  SellerOrder.findAllSellerOrderBySeller(req.body.id, function (err, order) {
+    if (err) {
+      console.log("errors" + err);
+      res.sendStatus(400);
+      return;
+    } else {
+      console.log(order);
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ order: order }));
+    }
+  });
+});
+
+router.post('/getAllSellerOrderByCustomer', urlencodedParser, async (req, res) => {
+  SellerOrder.findAllSellerOrderByCustomer(req.body.id, function (err, order) {
+    if (err) {
+      console.log("errors" + err);
+      res.sendStatus(400);
+      return;
+    } else {
+      console.log(order);
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ order: order }));
+    }
+  });
+});
+
+
+router.post('/getAllSellerOrderByDateAndSID', urlencodedParser, async (req, res) => {
+  SellerOrder.findAllSellerOrderByDateAndSID(req.body.id, req.body.sDate, req.body.eDate, function (err, order) {
+    if (err) {
+      console.log("errors" + err);
+      res.sendStatus(400);
+      return;
+    } else {
+      console.log(order);
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ order: order }));
+    }
+  });
+});
+
+router.post("/updateSellerOrder", urlencodedParser, function (req, res) {
+
+
+  var SID = req.body.seller_id;
+  var CID = req.body.customer_id;
+  var item = req.body.item;
+  var qty = req.body.qty;
+  var isShipped = req.body.isShipped;
+  var date = req.body.order_date;
+
+  var newOrder = {
+    seller_id: SID,
+    customer_id: CID,
+    item: item,
+    qty: qty,
+    isShipped: isShipped,
+    order_date: date,
+  };
+
+  let newOrder2 = Object.fromEntries(Object.entries(newOrder).filter(([_, v]) => v != null));
+
+  SellerOrder.updateSellerOrder(req.body.id, newOrder2, function (err, order) {
+    if (err) {
+      console.log("errors" + err);
+      res.sendStatus(400);
+      return;
+    } else {
+      console.log(order);
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ order: order }));
+    }
+  });
+});
+
+
+router.post("/deleteSellerOrder", urlencodedParser, function (req, res) {
+  console.log("Order Deleting");
+  var id = req.body.id;
+
+  SellerOrder.deleteSellerOrder(id, function (err, order) {
+    if (err) {
+      console.log("errors" + err);
+      res.sendStatus(400);
+      return;
+    } else {
+      console.log(order);
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ order: order }));
+    }
+  });
+});
+
 
 module.exports = router;
